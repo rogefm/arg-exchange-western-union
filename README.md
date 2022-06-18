a2babc6d6139640c63bb711b93b6f4b9311a098e
Rogerio Moura <roger0.rm@gmail.com>
2022-06-17 22:54:23 -0300

    feat(scrapper): create scrapper

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# arg-exchange-western-union
-Get currency exchange of Argentine pesos in Reals from Western Union
+# Scrapper to get currency exchange of Argentine pesos in Reals from Western Union
+
+## How to config
+
+1. Go to config.js.example and change the values.
+   1. Add your api key from scrapingbee. (to get access https://www.scrapingbee.com/)
+   2. Add your telegram bot api key. (More informations access https://core.telegram.org/bots#1-what-can-i-do-with-bots)
+
+## How to start
+
+1. `npm install`
+2. `node index.js`
+3. `have fun!`
---
+++ index.js
@@ -0,0 +1,60 @@
+const scrapingbee = require('scrapingbee');
+const { configs } = require('./config');
+
+async function get(url) {
+  var client = new scrapingbee.ScrapingBeeClient(configs.apiKey);
+  var response = await client.get({
+    url: url,
+    params: {
+      extract_rules: {
+        exchange: {
+          selector:
+            '#body-component > section.container-fuild.container-fuild_mb-fix.bk-action-light > div > div > div > div.row.calculator > div.col.col-16.col-M-8.pad-xs-tp > div > p > strong > span',
+          type: 'item',
+        },
+      },
+      js_scenario: {
+        instructions: [
+          { wait: 1000 },
+          {
+            wait_for:
+              '#body-component > section.container-fuild.container-fuild_mb-fix.bk-action-light > div > div > div > div.row.calculator > div.col.col-16.col-M-8.pad-xs-tp > div > p > strong > span',
+          },
+        ],
+      },
+    },
+  });
+  return response;
+}
+
+(async function main() {
+  try {
+    const response = await get('https://www.westernunion.com/br/es/currency-converter/brl-to-ars-rate.html');
+    const decoder = new TextDecoder();
+    const text = JSON.parse(decoder.decode(response.data));
+
+    fetch(`https://api.telegram.org/bot${configs.telegramBotApiKey}/sendMessage`, {
+      method: 'POST',
+      body: JSON.stringify({
+        chat_id: '289758835',
+        text: `Olá! hoje a cotação de 1 real para pesos argentino está ${text.exchange}`,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  } catch {
+    fetch(`https://api.telegram.org/bot${configs.telegramBotApiKey}/sendMessage`, {
+      method: 'POST',
+      body: JSON.stringify({
+        chat_id: '289758835',
+        text: `Ocorreu uma falha ao tentar buscar a contação de hoje.`,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+})();
